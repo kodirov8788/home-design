2f4c35e96d674b7978cfe529ea40c94ee350cb91
khamidovDev1 <khamidovsamandar5@gmail.com>
2023-03-21 14:24:12 +0500

    sam

---
+++ src/components/main/Main1.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import "./Main1.css"
+
+function Main1() {
+    return (
+        <div className='main1'>
+            <div className="main1_half1">
+                <div className="half1_text">
+                    <h3>Architecture design</h3>
+                    <h1>Professional Interior Design</h1>
+                    <p>Design for now with the awareness of yesterday and unknown tomorrow. Because small details make the perfect design. Helping you choose between good and best.</p>
+                    <button>Let’s work together</button>
+                </div>
+            </div>
+            <div className="main1_half2">
+
+            </div>
+
+        </div>
+    )
+}
+
+export default Main1
