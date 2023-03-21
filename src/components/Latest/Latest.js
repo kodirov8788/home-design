aba980fa089a9d6c359cfc8637ea4bb7011fb448
kodirov8788 <kodirov8788@gmail.com>
2023-03-21 14:18:11 +0500

    added abdullo with fixed

--- src/components/Latest/Latest.js
+++ src/components/Latest/Latest.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import "./Latest.css"
-import img1 from "../images/img1.png"
-import img2 from "../images/img2.png"
-import img3 from "../images/img3.png"
-import img4 from "../images/img4.png"
-import img5 from "../images/img5.png"
-import img6 from "../images/img6.png"
-import img7 from "../images/img7.png"
-import img8 from "../images/img8.png"
+import img1 from "../../images/img1.png"
+import img2 from "../../images/img2.png"
+import img3 from "../../images/img3.png"
+import img4 from "../../images/img4.png"
+import img5 from "../../images/img5.png"
+import img6 from "../../images/img6.png"
+import img7 from "../../images/img7.png"
+import img8 from "../../images/img8.png"
 function Latest() {
     return (
         <div className='lastest'>
